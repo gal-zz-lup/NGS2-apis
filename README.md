a5265789576797b5e1c705043bb1c85c1890b451
Matt Hoover <matthew.a.hoover@gmail.com>
2017-08-19 18:03:33 -0400

    Add SMS processing through Twilio.
    Tried to migrate this branch from another repo, but could only do
    that for one branch, not two. Therefore, losing the commit history
    by adding files manually, but all work here. Commit history was
    short (and uninformative) to begin with, so this should be fine.
    
    Code, tests, requirements, and README all up-to-date.

--- README.md
+++ README.md
@@ -1,7 +1,54 @@
-# NGS2-apis
+# NGS2-APIs
+This repository contains applications to handle various tasks through publicly available APIs, utilizing custom wrappers and SDKs.
 
-This repository is a place holder for applications that are written to handle various tasks with the use of publically available SDK(s) and Api(s). 
-
-## Payments with PayPal
+## Payment processing with PayPal
 
 ## Text Messages with Twillo
+### Introduction
+As part of World Labs, Gallup will be sending participants SMS reminders before their experiments via [Twilio](https://www.twilio.com) APIs. This allows flexibility in programming reminders and ensuring participant participation.
+
+### Setup
+Specifically, this repository uses the [Twilio SDK for Python](https://github.com/twilio/twilio-python) to wrap up messages for delivery. To execute these calls, a few pieces of information are necessary:
+1. A .csv file with the numbers to send messages to.
+2. A .txt file with the message text to be sent.
+3. REST API keys to authenticate against the API.
+
+#### Content details
+##### Phone numbers
+The .csv containing phone numbers to send messages to should be structured as follows:
+* `ExternalDataReference`: A alphanumeric field with the participant's unique identifying number.
+* `SMS_PHONE_CLEAN`: A numeric field with the participant's phone number (without country code).
+
+##### Message text
+The .txt file should contain nothing but the text that is to be sent to participants. For example, the file could be a .txt that consisted of just the following: `This is the message to send.`
+
+#### Running the program
+The processing program is written in Python and can be called from the command line. It takes four required arguments:
+* `-a` or `--auth`: **This argument requires three inputs.** The first is the Twilio REST API key for the account and the second is the Twilio REST API secret. Finally, the third is the sending phone number associated with the Twilio account. These are all assigned/designated once signing up for a Twilio developer account.
+* `-c` or `--content`: This argument is a .txt file with the text be to sent by SMS.
+* `-n` or `--nation`: This indicates to which country the SMS messages will be sent. Currently, only implemented for the United States (enter `US`). `
+* `-p` or `--phones`: This argument is a .csv fule with the phone number and participant IDs to whom SMS messages will be sent.
+
+#### Command-line Execution
+To execute the program, below is an example call:
+
+```
+$ python messaging/sms.py -a $TWILIO_ID $TWILIO_SECRET $TWILIO_PHONE \
+                          -c ~/Documents/ngs2/message_text.txt \
+                          -n US \
+                          -p ~/Documents/ngs2/sms_phones.csv
+```
+
+where `$TWILIO_ID`, `$TWILIO_SECRET`, and `$TWILIO_PHONE` are stored environmental variables with the appropriate REST API key/secret values.
+
+#### Logging
+The messaging program is set up with logging. Logging is important in being able to have a record of each time the program is run and what actually happened during the execution. The logging file is called `twilio_processing.log` and stored in the `messaging` folder of the repository. Each execution of the program will *append* to the log, not overwrite the last transaction, meaning a complete record of all executions is possible to have on disk.
+
+#### Testing
+This program includes a set of tests for the various functions that are being called through execution. They should be kept up-to-date as program functions change.
+
+### Troubleshooting
+If there are questions or problems, contact [Matt Hoover](matt_hoover@gallup.com) for assistance.
+
+### Conclusion
+This messaging program is a simple wrapper to send SMS messages using the Twilio APIs and SDK. If needed, it can be expanded upon and used for other means.
---
+++ messaging/sms.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+import argparse
+import logging
+import os
+import pandas as pd
+import re
+import sys
+import time
+
+from twilio.rest import Client
+
+
+logger = logging.getLogger(__name__)
+log_format = '%(asctime)s | %(name)s | %(filename)s (%(lineno)d) | %(levelname)s | %(message)s'
+logging.basicConfig(filename='messaging/twilio_processing.log', format=log_format,
+                    level=logging.DEBUG)
+logging.getLogger('urllib3').setLevel(logging.INFO)
+
+
+def format_phone_numbers(data, ctry):
+    if ctry == 'US':
+        return [(pid, '+1{}'.format(nbr)) for pid, nbr in data]
+    elif ctry == 'MA':
+        pass
+    elif ctry == 'PH':
+        pass
+    else:
+        pass
+
+
+def log_length_issues(data, ctry):
+    if ctry == 'US':
+        valid_nbrs = [(pid, phone) for pid, phone in data if
+                      len(str(phone)) == 10]
+        if len(valid_nbrs) != len(data):
+            logger.info('Not all numbers valid length; {} numbers being dropped \
+                        before sending.'.format(len(data[1]) - len(valid_nbrs[1])))
+        return valid_nbrs
+    elif ctry == 'MA':
+        pass
+    elif ctry == 'PH':
+        pass
+    else:
+        pass
+
+
+def log_numeric_issues(df):
+    clean_nbrs = [
+        (pid, phone) for pid, phone in
+        zip(df.ExternalDataReference, df.SMS_PHONE_CLEAN) if
+        isinstance(phone, int)
+    ]
+    if len(clean_nbrs) != len(df.SMS_PHONE_CLEAN):
+        logger.info('Not all numbers numeric; {} numbers being dropped before \
+                    sending.'.format(len(df.SMS_PHONE_CLEAN) - len(clean_nbrs[1])))
+    return clean_nbrs
+
+
+def msg_exists_test(content):
+    assert content, 'STOP! Message content is empty.'
+
+
+def phone_checks(df):
+    numeric_numbers = log_numeric_issues(df)
+    valid_numbers = log_length_issues(numeric_numbers, args_dict['nation'])
+    return valid_numbers
+
+
+def run(args_dict):
+    # start logger
+    logger.info('Starting transactions for {}.'.format(args_dict['phones']))
+
+    # load message content
+    with open(args_dict['content'], 'r') as f:
+        msg_content = f.read().strip('\n')
+
+    # test message content
+    msg_exists_test(msg_content)
+
+    # load phone number worksheet
+    d = pd.read_csv(args_dict['phones'], sep=None, engine='python')
+
+    # test phone numbers
+    valid_numbers = phone_checks(d)
+
+    # format phone numbers
+    formatted_numbers = format_phone_numbers(valid_numbers, args_dict['nation'])
+
+    # authenticate client
+    twilio = Client(args_dict['auth'][0], args_dict['auth'][1])
+
+    # send messages
+    msgs = [
+        twilio.messages.create(
+            to=phone,
+            from_=args_dict['auth'][2],
+            body=msg_content,
+        ) for _, phone in formatted_numbers
+    ]
+    logger.info('Processed {} messages.'.format(len(msgs)))
+
+    # sleep for one minute to queue messaging status for return
+    time.sleep(60)
+
+    # log delivery code
+    delivery = pd.DataFrame(
+        [(pid, msg.fetch().status, msg.sid) for pid, msg in
+         zip([pid for pid, _ in formatted_numbers], msgs)],
+        columns=['ExternalDataReference', 'MessageStatus', 'MessageReference'],
+    )
+
+    # merge delivery code back to input data
+    d = d.merge(delivery, on='ExternalDataReference', how='left')
+
+    # # output data
+    fileparts = os.path.splitext(args_dict['phones'])
+    d.to_csv('{}_delivery{}'.format(fileparts[0], fileparts[1]), index=False)
+    logger.info('Closing log for {}.\n'.format(args_dict['phones']))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Send SMS via Twilio.')
+    parser.add_argument('-a', '--auth', required=True, nargs=3, help='API '
+                        'authorization SID, token, and sending phone number '
+                        '(in that order).')
+    parser.add_argument('-c', '--content', required=True, help='Path/file to '
+                        'TXT with message content information.')
+    parser.add_argument('-n', '--nation', required=True,
+                        choices=['US'], help='Country 2-letter ISO '
+                        'code for recipients.')
+    parser.add_argument('-p', '--phones', required=True, help='Path/file to '
+                        'CSV with phone delivery information.')
+    args_dict = vars(parser.parse_args())
+
+    run(args_dict)
